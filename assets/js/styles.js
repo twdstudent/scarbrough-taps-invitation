37dbeea6e2873214cc14d73e4d83d416d5259fb1
Ubuntu <ubuntu@ip-172-31-4-175.eu-west-1.compute.internal>
2019-09-19 17:40:52 +0000

    added more links

---
+++ assets/js/styles.js
@@ -0,0 +1,11 @@
+<script>
+    function printRecipe() {
+        var print_div = document.getElementById("recipeArea");
+        var print_area = window.open();
+        print_area.document.write(print_div.innerHTML);
+        print_area.document.close();
+        print_area.focus();
+        print_area.print();
+        print_area.close();
+    }
+</script>
